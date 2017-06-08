457f0dfee26a8f536bf1a27be15dd3cde9fcf0b2
Ian Kirkpatrick <ianmann56@gmail.com>
2017-06-08 18:55:33 -0400

    Worked on Neuron
    Neuron now has axon, a 2D ArrayList of NeuralPathways. I also added a
    storage manager. Neuron uses this and i plan on adding it to
    NeuralPathway and anything else that will be stored.

--- src/com/ianmann/mind/Neuron.java
+++ src/com/ianmann/mind/Neuron.java
@@ -12,6 +12,7 @@ import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.io.Serializable;
 import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -30,6 +31,7 @@ import com.ianmann.mind.storage.organization.basicNetwork.Description;
 import com.ianmann.mind.storage.organization.basicNetwork.EntityStructure;
 import com.ianmann.mind.storage.organization.basicNetwork.NeuralNetwork;
 import com.ianmann.mind.utils.Serializer;
+import com.ianmann.utils.storage.StorageManageable;
 import com.ianmann.utils.utilities.Files;
 import com.ianmann.utils.utilities.GeneralUtils;
 import com.ianmann.utils.utilities.JSONUtils;
@@ -43,25 +45,25 @@ import com.ianmann.utils.utilities.JSONUtils;
 public class Neuron extends File {
 	
 	/**
+	 * Contains CRUD operations for the Neuron class. This class
+	 * implements the interface {@link StorageManageable}.
+	 */
+	public static NeuronManager storage = new NeuronManager();
+	
+	/**
 	 * Denotes the structural layout of the network of neurons
 	 * connected to this neuron. Examples of this may be noun
 	 * structures or noun instances.
 	 */
 	protected int type;
-
-	/**
-	 * Category that is the parent of this one.
-	 * If null, this is a root category.
-	 */
-	protected File parentCategory;
 	
 	/**
 	 * Groups of postsynaptic dendrites. These are the connections to another
 	 * Neuron. They are grouped so that the networks can be parsed. For
-	 * example, one list in this hashmap may be a collection of attributes
+	 * example, one list in this list may be a collection of attributes
 	 * where as another list may be a collection of abilities.
 	 */
-	protected HashMap<Integer, ArrayList<File>> axon;
+	protected ArrayList<ArrayList<NeuralPathway>> axon;
 	
 	/**
 	 * Used by developers or other users looking into the AI
@@ -73,7 +75,12 @@ public class Neuron extends File {
 	protected String associatedMorpheme;
 	
 	/**
-	 * Used for parsing json into a neuron object.
+	 * Instantiates a Neuron with the path to a file that currently
+	 * contains a Neuron's data.
+	 * 
+	 * It is assumed that this neuron already exists in memory. This
+	 * constructor merely wraps it in a Neuron class for use in the
+	 * program.
 	 */
 	protected Neuron(String _path) {
 		super(_path);
@@ -87,22 +94,10 @@ public class Neuron extends File {
 	 * @param _linkedThought
 	 * @param _associated
 	 */
-	protected Neuron(String _path, int _type, EmotionUnit _associated, String _label, Category _category) {
+	protected Neuron(String _path, int _type, String _label) {
 		super(_path);
 		this.associatedMorpheme = _label;
-		this.initialize(_type, _associated, _label, _category);
-	}
-	
-	public static Neuron create(int _type, EmotionUnit _associated, Category _category) {
-		String location = getNewFileLocation();
-		Neuron neuron = new Neuron(location, _type, _associated, null, _category);
-		return neuron;
-	}
-	
-	public static Neuron create(int _type, EmotionUnit _associated, String _label, Category _category) {
-		String location = getNewFileLocation();
-		Neuron neuron = new Neuron(location, _type, _associated, _label, _category);
-		return neuron;
+		this.initialize(_type, _label);
 	}
 	
 	/**
@@ -111,51 +106,12 @@ public class Neuron extends File {
 	 * @param _associated
 	 * @param _label
 	 */
-	private void initialize(int _type, EmotionUnit _associated, String _label, Category _category) {
+	private void initialize(int _type, String _label) {
 		this.type = _type;
 		
-		this.setParentCategory(_category);
-		this.synapticEndings = new ArrayList<File>();
+		this.axon = new ArrayList<ArrayList<NeuralPathway>>();
 		
 		this.setAssociatedMorpheme(_label);
-		this.associatedEmotion = _associated;
-	}
-	
-	/**
-	 * Sets the parent neuron for this neuron. _neuron's type must be the same
-	 * as this neurons type.
-	 * @param _neuron
-	 */
-	public void setParentNeuron(Neuron _neuron) {
-		if (_neuron.getType() != this.getType()) {
-			// TODO: throw exception because you can't set this neurons
-			// parent to a neuron of a different type.
-			return;
-		} else {
-			if (this.parentNeuron != null) {
-				if (this.parentNeuron.delete()) {
-					NeuralPathway t = new NeuralPathway(_neuron.location);
-					this.parentNeuron = t.location;
-					this.save();
-				}
-			} else {
-				NeuralPathway t = new NeuralPathway(_neuron.location);
-				this.parentNeuron = t.location;
-				this.save();
-			}
-		}
-	}
-	
-	/**
-	 * Returns the Neuron that represents the parent neuron for this neuron.
-	 * @return
-	 */
-	public Neuron getParentNeuron() {
-		if (this.parentNeuron != null) {
-			return NeuralPathway.deserialize(this.parentNeuron).fireSynapse();
-		} else {
-			return null;
-		}
 	}
 	
 	/**
@@ -168,13 +124,13 @@ public class Neuron extends File {
 	}
 	
 	/**
-	 * Returns all synaptic endings related to this neuron. Each file in
-	 * this list contains a NeuralPathway object. Use fireSynapse() on
-	 * the objects to get the actual Neuron object.
+	 * Returns all synaptic endings related to this neuron.
+	 * Use fireSynapse() on the objects to get the actual
+	 * Neuron object.
 	 * @return
 	 */
-	public ArrayList<File> getSynapticEndings() {
-		return this.synapticEndings;
+	public ArrayList<ArrayList<NeuralPathway>> getAxon() {
+		return this.axon;
 	}
 	
 	/**
@@ -194,124 +150,45 @@ public class Neuron extends File {
 	}
 	
 	/**
-	 * Return the parent Category of this neuron from it's file.
-	 * @return
-	 */
-	public Category getParentCategory() {
-		try {
-			return Category.parse(this.parentCategory);
-		} catch (FileNotFoundException | ParseException e) {
-			return null;
-		}
-	}
-	
-	/**
-	 * Set the parent Category of this neuron.
-	 * @param _category
-	 */
-	public void setParentCategory(Category _category) {
-		if (_category == null) {
-			return;
-		} else {
-			this.parentCategory = _category.location;
-			Neuron c = this.getParentCategory();
-			c.addNeuralPathway(this);
-			c.save();
-		}
-	}
-	
-	/**
-	 * Moves this neuron to the category represented
-	 * by _category. This method also saves _category
-	 * if it's not null or the same category as it use
-	 * to be assimilated with. It also saves this neuron.
-	 * @param _category
-	 */
-	public void assimilate(Category _category) {
-		// If this neuron has a parent category already and it's not the same as _category
-		if (this.parentCategory != null && !this.getParentCategory().equals(_category)) {
-			// Remove this file from this.parentCategory's folder.
-			this.location.delete();
-			this.location = null; // Set it null so that getFileLocation() will do logic to get the new path.
-			Neuron c = this.getParentCategory();
-			c.removeNeuralPathway(this);
-			c.save();
-		}
-		this.setParentCategory(_category);
-		// Store this neuron in _category
-		this.location = new File(this.getFileLocation());
-		this.save();
-	}
-	
-	/**
-	 * Retrieve the location to the file containing this Neuron.
+	 * Retrieve the location to the file containing a new Neuron.
 	 * <br><br>
 	 * This method does the logic for deciding what to name the file.
-	 * If no associated morpheme is found for this neuron, it will
+	 * If no label is provided in the parameters, it will
 	 * use an id from neuron ids file.
+	 * <br><br>
+	 * NOTE: _label is optional. If it is null, then the next
+	 * id will be used instead of _label.
 	 */
-	protected static String getNewFileLocation() {
-		// Get category folder
-		String pathToCategory = "";
-		if (this.parentCategory != null) {
-			pathToCategory = this.getParentCategory().getCategoryPath();
-		} else {
-			// no category so just set pathToCategory to the root neuron folder
-			pathToCategory = Constants.NEURON_ROOT;
-		}
-		
-		if (this.associatedMorpheme == null) {
-			/*
-			 * If no morpheme is found for this neuron,
-			 * grab the id out of id file and use that for
-			 * the file name. Then increment the next id.
-			 */
-			Scanner s;
-			try {
+	protected static String getNewFileLocation(String _label) {
+		String pathToNeurons = Constants.NEURON_ROOT;
+		try {
+			if (_label != null) {
+				return pathToNeurons + _label + ".nrn";
+			} else {
+				Scanner s;
 				s = new Scanner(new File(Constants.NEURON_ROOT + "ids"));
 				int next = s.nextInt();
 				s.close();
 				PrintWriter p = new PrintWriter(new File(Constants.NEURON_ROOT + "ids"));
 				p.print(next+1);
 				p.close();
-				return pathToCategory + String.valueOf(next) + ".nrn";
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				return null;
+				return pathToNeurons + String.valueOf(next) + ".nrn";
 			}
-		} else {
-			/*
-			 * A morpheme is saved for this neuron so just use
-			 * that as the file name.
-			 */
-			return pathToCategory + this.associatedMorpheme + ".nrn";
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
 		}
-	}
-	
-	/**
-	 * Return the index at which _neuron is stored in
-	 * {@code this.SynapticEndings}.
-	 * @param _neuron
-	 * @return
-	 */
-	private int indexOfNeuralPathway(Neuron _neuron) {
-		for (int i = 0; i < this.synapticEndings.size(); i++) {
-			if (this.synapticEndings.get(i).equals(_neuron)) {
-				return i;
-			}
-		}
-		return -1;
 	}
 	
 	/**
 	 * Make new pathway to a thought. This automatically saves the changes to storage.
 	 * @param _thought
 	 */
-	public NeuralPathway addNeuralPathway(Neuron _thought) {
+	public NeuralPathway addNeuralPathway(Integer _dendriteGroup, Neuron _thought) {
 		if (_thought != null) {
-			NeuralPathway t = new NeuralPathway(_thought.location);
-			this.synapticEndings.add(t.location);
+			NeuralPathway t = new NeuralPathway(_thought);
+			this.axon.get(_dendriteGroup).add(t);
 			this.save();
 			return t;
 		} else {
@@ -320,50 +197,21 @@ public class Neuron extends File {
 	}
 	
 	/**
-	 * Remove the pathway to a thought.
+	 * Remove the pathway to a thought. If no pathway is found at this location,
+	 * nothing happens.
 	 * @param _thought
 	 */
-	public void removeNeuralPathway(Neuron _thought) {
-		if (_thought != null) {
-			int indexOfPathway = this.indexOfNeuralPathway(_thought);
-			if (indexOfPathway != -1) {
-				NeuralPathway pathway = NeuralPathway.deserialize(this.synapticEndings.get(indexOfPathway));
-				if (pathway.location.delete()) {
-					this.synapticEndings.remove(indexOfPathway);
-					this.save();
-				}
-			} else {
-				return;
+	public void removeNeuralPathway(int _dendriteGroupIndex, int _indexInGroup) {
+		NeuralPathway pathway = NeuralPathway.deserialize(this.axon.get(_dendriteGroupIndex).get(_indexInGroup));
+		if (pathway != null) {
+			if (pathway.delete()) {
+				this.axon.get(_dendriteGroupIndex).remove(_indexInGroup);
+				this.save();
 			}
-		}
-	}
-	
-	/**
-	 * Return a list of neurons that are associated with this neuron.
-	 * The neurons returned will be stored in the folder or subfolder
-	 * of the folder represented by the given path in _category.
-	 * 
-	 * @param _category - Neuron with path to folder that contains the
-	 * Neurons desired. returns neurons in subfolders of _category as well.
-	 */
-	@Deprecated
-	public ArrayList<Neuron> getRelatedNeuronsInCategory(Category _category) {
-		String categoryFolder = _category.getCategoryPath();
-		ArrayList<Neuron> neurons = new ArrayList<Neuron>();
-		
-		for (File neuronFile : this.synapticEndings) {
-			String pathFromNeuronRoot = neuronFile.getAbsolutePath().split(Constants.NEURON_ROOT)[1];
-			if (pathFromNeuronRoot.startsWith(categoryFolder)) {
-				try {
-					neurons.add(Neuron.fromJSON(neuronFile));
-				} catch (FileNotFoundException | ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+		} else {
+			return;
 		}
 		
-		return neurons;
 	}
 	
 	public NeuralNetwork parsed() {
@@ -379,47 +227,23 @@ public class Neuron extends File {
 	}
 	
 	/**
-	 * Remove the neuron file that stores this neuron.
-	 */
-	protected void destroy() {
-		this.location.delete();
-		this.location = null;
-	}
-	
-	/**
 	 * Print this object to the file at {@link Neuron.location}.
 	 * <br><br>
 	 * If the neuron file already exists, just rewrite the data
 	 * in the file, overwriting the old data with the new data.
 	 */
 	public void save() {
-		try {
-			try {
-				if (!this.location.exists()) {
-					java.nio.file.Files.createFile(this.location.toPath());
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			PrintWriter objWriter = new PrintWriter(this.location);
-			objWriter.print(JSONUtils.formatJSON(this.jsonify(), 0));
-			objWriter.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Neuron.storage.save(this);
 	}
 	
 	/**
-	 * Determines whether this neuron is the same as that in o. This is true if o.location is
-	 * the same file as this.location.
+	 * Determines whether this neuron is the same as that in o. This is true if o's path is
+	 * the same as this Neuron's path.
 	 * @param o
 	 * @return
 	 */
 	public boolean equals(Neuron o) {
-		return this.location.equals(o.location);
+		return this.getAbsolutePath().equals(o.getAbsolutePath());
 	}
 	
 	/**
@@ -429,45 +253,27 @@ public class Neuron extends File {
 	 * @throws FileNotFoundException
 	 * @throws ParseException
 	 */
-	public static Neuron fromJSON(File _neuronFile) throws FileNotFoundException, ParseException {
-		JSONObject jsonNeuron = (JSONObject) Files.json(_neuronFile);
+	private void readFile() throws FileNotFoundException, ParseException {
+		JSONObject jsonNeuron = (JSONObject) Files.json(this);
 		
-		Neuron n = new Neuron();
-		
-		String parentCategoryString = (String) jsonNeuron.get("parentCategory");
-		if (!parentCategoryString.equals("NO_CATEGORY")) {
-			n.parentCategory = new File(Constants.STORAGE_ROOT + (String) jsonNeuron.get("parentCategory"));
-		} else {
-			n.parentCategory = null;
+		this.axon = new ArrayList<ArrayList<NeuralPathway>>();
+		JSONArray axon = (JSONArray) jsonNeuron.get("axon");
+		for (int i; i < axon.size(); i++) {
+			JSONArray dendriteGroup = (JSONArray) axon.get(i);
+			this.axon.add(new ArrayList<NeuralPathway>());
+			for (Object path : dendriteGroup) {
+				String filePath = Constants.STORAGE_ROOT + (String) path;
+				this.axon.get(i).add(new NeuralPathway(filePath, false));
+			}
 		}
 		
-		String parentNeuronString = (String) jsonNeuron.get("parentNeuron");
-		if (!parentNeuronString.equals("NO_PARENT")) {
-			n.parentNeuron = new File(Constants.STORAGE_ROOT + (String) jsonNeuron.get("parentNeuron"));
-		} else {
-			n.parentNeuron = null;
-		}
-		
-		n.synapticEndings = new ArrayList<File>();
-		JSONArray synaptics = (JSONArray) jsonNeuron.get("synapticEndings");
-		for (Object path : synaptics) {
-			String filePath = Constants.STORAGE_ROOT + (String) path;
-			n.synapticEndings.add(new File(filePath));
-		}
-		
-		n.location = new File(Constants.STORAGE_ROOT + (String) jsonNeuron.get("location"));
-		
-		n.associatedEmotion = EmotionUnit.getEmotion((String) jsonNeuron.get("associatedEmotion"));
-		
-		n.type = (int) ((long) jsonNeuron.get("type"));
+		this.type = (int) ((long) jsonNeuron.get("type"));
 		
 		if (!(jsonNeuron.get("associatedMorpheme") instanceof Long)) {
-			n.associatedMorpheme = (String) jsonNeuron.get("associatedMorpheme");
+			this.associatedMorpheme = (String) jsonNeuron.get("associatedMorpheme");
 		} else {
-			n.associatedEmotion = null;
+			this.associatedMorpheme = null;
 		}
-		
-		return n;
 	}
 	
 	/**
@@ -478,25 +284,20 @@ public class Neuron extends File {
 	public JSONObject jsonify() {
 		JSONObject neuronJson = new JSONObject();
 		
-		if (this.parentCategory != null) {
-			neuronJson.put("parentCategory", this.parentCategory.getAbsolutePath().split(Constants.STORAGE_ROOT)[1]);
-		} else {
-			neuronJson.put("parentCategory", "NO_CATEGORY");
+		neuronJson.put("axon", new JSONArray());
+		for (int i = 0; i < this.axon.size(); i++) {
+			ArrayList<NeuralPathway> dendriteGroup = this.axon.get(i);
+			((JSONArray) neuronJson.get("axon")).add(new JSONArray());
+			for (NeuralPathway synapse : dendriteGroup) {
+				(
+					(JSONArray) ((JSONArray) neuronJson.get("axon"))
+						.get(i)
+				).add(synapse.getAbsolutePath().split(Constants.STORAGE_ROOT)[1]);
+			}
 		}
 		
-		if (this.parentNeuron != null) {
-			neuronJson.put("parentNeuron", this.parentNeuron.getAbsolutePath().split(Constants.STORAGE_ROOT)[1]);
-		} else {
-			neuronJson.put("parentNeuron", "NO_PARENT");
-		}
-		
-		neuronJson.put("synapticEndings", new JSONArray());
-		for (File synapse : this.synapticEndings) {
-			((JSONArray) neuronJson.get("synapticEndings")).add(synapse.getAbsolutePath().split(Constants.STORAGE_ROOT)[1]);
-		}
-		neuronJson.put("location", this.location.getAbsolutePath().split(Constants.STORAGE_ROOT)[1]);
-		neuronJson.put("associatedEmotion", this.associatedEmotion.getName());
 		neuronJson.put("type", this.type);
+		
 		if (this.associatedMorpheme != null) {
 			neuronJson.put("associatedMorpheme", this.associatedMorpheme);
 		} else {
@@ -514,4 +315,97 @@ public class Neuron extends File {
 		str = str + ">";
 		return str;
 	}
+}
+
+class NeuronManager implements StorageManageable<Neuron> {
+
+	/**
+	 * The parameters for this method should be treated like so:
+	 * (Integer _type, String _associatedMorpheme)
+	 * This is what the method expects in it's parameters.
+	 * 
+	 *  (non-Javadoc)
+	 * @see com.ianmann.utils.storage.StorageManageable#create()
+	 */
+	@Override
+	public Neuron create(Object... _params) {
+		String location = Neuron.getNewFileLocation((String) _params[1]);
+		Neuron neuron = new Neuron(
+				location,
+				(Integer) _params[0],
+				(String) _params[1]
+		);
+		return neuron;
+	}
+
+	/**
+	 * Print this object to the file at {@link Neuron.location}.
+	 * <br><br>
+	 * If the neuron file already exists, just rewrite the data
+	 * in the file, overwriting the old data with the new data.
+	 * 
+	 * (non-Javadoc)
+	 * @see com.ianmann.utils.storage.StorageManageable#save(java.lang.Object)
+	 */
+	@Override
+	public void save(Neuron _object) {
+		try {
+			try {
+				if (!_object.exists()) {
+					java.nio.file.Files.createFile(_object.toPath());
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			PrintWriter objWriter = new PrintWriter(_object);
+			objWriter.print(JSONUtils.formatJSON(_object.jsonify(), 0));
+			objWriter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Removes this Neuron from memory. First, this will delete all of the
+	 * NeuralPathway objects that are contained in this Neuron. Then the actual
+	 * Neuron file will be deleted.
+	 * 
+	 * (non-Javadoc)
+	 * @see com.ianmann.utils.storage.StorageManageable#delete(java.lang.Object)
+	 */
+	@Override
+	public void delete(Neuron _object) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < _object.axon.size(); i++) {
+			for (int j = 0; j < _object.axon.get(i).size(); j++) {
+				_object.removeNeuralPathway(i, j);
+			}
+		}
+		_object.delete();
+	}
+
+	/**
+	 * DON'T USE THIS!
+	 * (non-Javadoc)
+	 * @see com.ianmann.utils.storage.StorageManageable#get(java.util.HashMap)
+	 */
+	@Override
+	public ArrayList<Neuron> get(HashMap<String, Object> _params) {
+		return null;
+	}
+
+	/** 
+	 * DON'T USE THIS!
+	 * (non-Javadoc)
+	 * @see com.ianmann.utils.storage.StorageManageable#getAll()
+	 */
+	@Override
+	public ArrayList<Neuron> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
---
+++ src/com/ianmann/utils/storage/StorageManageable.java
@@ -0,0 +1,63 @@
+/**
+ * @TODO: TODO
+ *
+ * @author Ian
+ * Created: Jun 6, 2017
+ */
+package com.ianmann.utils.storage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Provides an interface with methods for CRUD operations on a given class.
+ * 
+ * @TODO: TODO
+ *
+ * @author Ian
+ * Created: Jun 6, 2017
+ */
+public interface StorageManageable<T> {
+
+	/**
+	 * Create and save to storage an instance of T.
+	 * The instance will have values of _params.
+	 * @return
+	 */
+	public T create(Object... _params);
+	
+	/**
+	 * Save an instance of T specified by _object. This
+	 * is specifically meant to be used when updating
+	 * an object and it needs to take affect in the
+	 * place of storage where the instance is stored.
+	 * @param _object
+	 */
+	public void save(T _object);
+	
+	/**
+	 * Delete an instance of T specified by _object. This
+	 * will remove this instance from storage.
+	 * @param _object
+	 */
+	public void delete(T _object);
+	
+	/**
+	 * Get a list of instances of T. The instances will
+	 * fulfill the conditions stated in _params. For
+	 * example if one parameter is "name": "Johnson",
+	 * then any instance with the "name" equal to
+	 * "Johnson" will be returned.
+	 * @param _params
+	 * @return
+	 */
+	public ArrayList<T> get(HashMap<String, Object> _params);
+	
+	/**
+	 * Returns all instance of T that are valid. The
+	 * implementation of this will decide which objects
+	 * are valid.
+	 * @return
+	 */
+	public ArrayList<T> getAll();
+}
